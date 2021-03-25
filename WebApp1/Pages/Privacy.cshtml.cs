bfd5a1d46823f0270c701b9e4e7f51c91e704282
DotExe-mx <briyant_zeuz21@hotmail.com>
2021-03-25 15:54:40 -0600

    Scaffolding.

--- WebApp1/Pages/Privacy.cshtml.cs
+++ WebApp1/Pages/Privacy.cshtml.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebApp1.Pages
 {
+    [Authorize]
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
@@ -22,3 +20,4 @@ namespace WebApp1.Pages
         }
     }
 }
+
